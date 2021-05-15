ff4c7b301c735b23802f4ee48a9e83938664e0aa
Łukasz Furlepa <lucas.furlepa@gmail.com>
2021-05-15 18:02:52 +0200

    Add benchmarking project;

---
+++ benchmarks/LfrlSoft.NET.Common.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace LfrlSoft.NET.Common.Benchmarks
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<StructEqualityBenchmark>();
+            Console.ReadKey( true );
+        }
+    }
+}
