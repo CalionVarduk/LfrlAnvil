7cfe3bd3513dae799d9d75f8053263c9731ddf27
Łukasz Furlepa <lucas.furlepa@gmail.com>
2021-07-25 18:03:54 +0200

    Common: - Add MemberInfoExtensions; - Add ParameterInfoExtensions;

---
+++ src/LfrlSoft.NET.Common/Extensions/MemberInfoExtensions.s.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace LfrlSoft.NET.Common.Extensions
+{
+    public static class MemberInfoExtensions
+    {
+        public static Attribute? GetAttribute(this MemberInfo member, Type attributeType, bool inherit = true)
+        {
+            return Attribute.GetCustomAttribute( member, attributeType, inherit );
+        }
+
+        public static T? GetAttribute<T>(this MemberInfo member, bool inherit = true)
+            where T : Attribute
+        {
+            return (T?) member.GetAttribute( typeof( T ), inherit );
+        }
+
+        public static Attribute[] GetAttributeRange(this MemberInfo member, Type attributeType, bool inherit = true)
+        {
+            return Attribute.GetCustomAttributes( member, attributeType, inherit );
+        }
+
+        public static IEnumerable<T> GetAttributeRange<T>(this MemberInfo member, bool inherit = true)
+            where T : Attribute
+        {
+            return member.GetAttributeRange( typeof( T ), inherit ).Select( a => (T) a );
+        }
+
+        public static bool HasAttribute(this MemberInfo member, Type attributeType, bool inherit = true)
+        {
+            return Attribute.IsDefined( member, attributeType, inherit );
+        }
+
+        public static bool HasAttribute<T>(this MemberInfo member, bool inherit = true)
+            where T : Attribute
+        {
+            return member.HasAttribute( typeof( T ), inherit );
+        }
+
+        public static EventInfo? TryAsEvent(this MemberInfo member)
+        {
+            return member as EventInfo;
+        }
+
+        public static FieldInfo? TryAsField(this MemberInfo member)
+        {
+            return member as FieldInfo;
+        }
+
+        public static PropertyInfo? TryAsProperty(this MemberInfo member)
+        {
+            return member as PropertyInfo;
+        }
+
+        public static Type? TryAsType(this MemberInfo member)
+        {
+            return member as Type;
+        }
+
+        public static ConstructorInfo? TryAsConstructor(this MemberInfo member)
+        {
+            return member as ConstructorInfo;
+        }
+
+        public static MethodInfo? TryAsMethod(this MemberInfo member)
+        {
+            return member as MethodInfo;
+        }
+    }
+}
---
+++ src/LfrlSoft.NET.Common/Extensions/ParameterInfoExtensions.s.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace LfrlSoft.NET.Common.Extensions
+{
+    public static class ParameterInfoExtensions
+    {
+        public static Attribute? GetAttribute(this ParameterInfo parameter, Type attributeType, bool inherit = true)
+        {
+            return Attribute.GetCustomAttribute( parameter, attributeType, inherit );
+        }
+
+        public static T? GetAttribute<T>(this ParameterInfo parameter, bool inherit = true)
+            where T : Attribute
+        {
+            return (T?) parameter.GetAttribute( typeof( T ), inherit );
+        }
+
+        public static Attribute[] GetAttributeRange(this ParameterInfo parameter, Type attributeType, bool inherit = true)
+        {
+            return Attribute.GetCustomAttributes( parameter, attributeType, inherit );
+        }
+
+        public static IEnumerable<T> GetAttributeRange<T>(this ParameterInfo parameter, bool inherit = true)
+            where T : Attribute
+        {
+            return parameter.GetAttributeRange( typeof( T ), inherit ).Select( a => (T) a );
+        }
+
+        public static bool HasAttribute(this ParameterInfo parameter, Type attributeType, bool inherit = true)
+        {
+            return Attribute.IsDefined( parameter, attributeType, inherit );
+        }
+
+        public static bool HasAttribute<T>(this ParameterInfo parameter, bool inherit = true)
+            where T : Attribute
+        {
+            return parameter.HasAttribute( typeof( T ), inherit );
+        }
+    }
+}
