3b92fc742f63484ef05930558887648085b033f2
Łukasz Furlepa <lucas.furlepa@gmail.com>
2021-09-05 20:46:16 +0200

    Core: - Add TimestampProvider & PreciseTimestampProvider classes;

---
+++ src/LfrlSoft.NET.Core/Chrono/ITimestampProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace LfrlSoft.NET.Core.Chrono
+{
+    public interface ITimestampProvider
+    {
+        Timestamp GetNow();
+    }
+}
---
+++ src/LfrlSoft.NET.Core/Chrono/TimestampProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace LfrlSoft.NET.Core.Chrono
+{
+    public sealed class TimestampProvider : ITimestampProvider
+    {
+        [Pure]
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        public Timestamp GetNow()
+        {
+            return new Timestamp( DateTime.UtcNow );
+        }
+    }
+}
