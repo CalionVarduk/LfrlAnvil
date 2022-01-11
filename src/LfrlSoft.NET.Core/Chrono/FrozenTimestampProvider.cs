77b167524e9c66e7544b30d63212d28a6c8a4eb9
Łukasz Furlepa <lucas.furlepa@gmail.com>
2022-01-11 17:30:58 +0100

    Core: - Add Chrono.FrozenTimestampProvider class; - Add Chrono.ITimestampProvider extensions;

---
+++ src/LfrlSoft.NET.Core/Chrono/FrozenTimestampProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace LfrlSoft.NET.Core.Chrono
+{
+    public sealed class FrozenTimestampProvider : ITimestampProvider
+    {
+        private readonly Timestamp _now;
+
+        public FrozenTimestampProvider(Timestamp now)
+        {
+            _now = now;
+        }
+
+        [Pure]
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        public Timestamp GetNow()
+        {
+            return _now;
+        }
+    }
+}
