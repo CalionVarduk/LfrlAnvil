721f9a1b32119baa0f261bc1579e26771e48739d
Łukasz Furlepa <lucas.furlepa@gmail.com>
2022-05-07 16:38:30 +0200

    Mapping: - Add TypeMapperBuilder class;

---
+++ src/LfrlAnvil.Mapping/TypeMapperBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace LfrlAnvil.Mapping
+{
+    public class TypeMapperBuilder : ITypeMapperBuilder
+    {
+        private readonly List<ITypeMappingConfiguration> _configurations;
+
+        public TypeMapperBuilder()
+        {
+            _configurations = new List<ITypeMappingConfiguration>();
+        }
+
+        public ITypeMapperBuilder Configure(ITypeMappingConfiguration configuration)
+        {
+            _configurations.Add( configuration );
+            return this;
+        }
+
+        public ITypeMapperBuilder Configure(params ITypeMappingConfiguration[] configurations)
+        {
+            return Configure( configurations.AsEnumerable() );
+        }
+
+        public ITypeMapperBuilder Configure(IEnumerable<ITypeMappingConfiguration> configurations)
+        {
+            _configurations.AddRange( configurations );
+            return this;
+        }
+
+        [Pure]
+        public IEnumerable<ITypeMappingConfiguration> GetConfigurations()
+        {
+            return _configurations;
+        }
+
+        [Pure]
+        public ITypeMapper Build()
+        {
+            return new TypeMapper( _configurations );
+        }
+    }
+}
---
+++ tests/LfrlAnvil.Mapping.Tests/TypeMapperBuilderTests/TypeMapperBuilderTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using LfrlAnvil.Mapping.Internal;
+using LfrlAnvil.TestExtensions;
+using LfrlAnvil.TestExtensions.FluentAssertions;
+using Xunit;
+
+namespace LfrlAnvil.Mapping.Tests.TypeMapperBuilderTests
+{
+    public class TypeMapperBuilderTests : TestsBase
+    {
+        [Fact]
+        public void Ctor_ShouldCreateEmptyBuilder()
+        {
+            var sut = new TypeMapperBuilder();
+            sut.GetConfigurations().Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Configure_ShouldAddFirstConfigurationCorrectly()
+        {
+            var configuration = TypeMappingConfiguration.Create<int, string>( (_, _) => string.Empty );
+            var sut = new TypeMapperBuilder();
+
+            var result = sut.Configure( configuration );
+
+            using ( new AssertionScope() )
+            {
+                result.Should().BeSameAs( sut );
+                sut.GetConfigurations().Should().BeSequentiallyEqualTo( configuration );
+            }
+        }
+
+        [Fact]
+        public void Configure_ShouldAddNextConfigurationCorrectly()
+        {
+            var configuration1 = TypeMappingConfiguration.Create<int, string>( (_, _) => string.Empty );
+            var configuration2 = TypeMappingConfiguration.Create<string, int>( (_, _) => default );
+            var sut = new TypeMapperBuilder();
+            sut.Configure( configuration1 );
+
+            var result = sut.Configure( configuration2 );
+
+            using ( new AssertionScope() )
+            {
+                result.Should().BeSameAs( sut );
+                sut.GetConfigurations().Should().BeSequentiallyEqualTo( configuration1, configuration2 );
+            }
+        }
+
+        [Fact]
+        public void Configure_WithCollection_ShouldAddConfigurationsCorrectly()
+        {
+            var configuration1 = TypeMappingConfiguration.Create<int, string>( (_, _) => string.Empty );
+            var configuration2 = TypeMappingConfiguration.Create<string, int>( (_, _) => default );
+            var configuration3 = TypeMappingConfiguration.Create<string, Guid>( (_, _) => default );
+            var sut = new TypeMapperBuilder();
+
+            var result = sut.Configure( configuration1, configuration2, configuration3 );
+
+            using ( new AssertionScope() )
+            {
+                result.Should().BeSameAs( sut );
+                sut.GetConfigurations().Should().BeSequentiallyEqualTo( configuration1, configuration2, configuration3 );
+            }
+        }
+
+        [Fact]
+        public void Build_ShouldReturnCorrectResult()
+        {
+            var configuration1 = TypeMappingConfiguration.Create<int, string>( (_, _) => string.Empty );
+            var configuration2 = TypeMappingConfiguration.Create<string, int>( (_, _) => default );
+            var configuration3 = TypeMappingConfiguration.Create<string, Guid>( (_, _) => default );
+            var sut = new TypeMapperBuilder();
+            sut.Configure( configuration1, configuration2, configuration3 );
+            var expectedKeys = new[]
+            {
+                new TypeMappingKey( typeof( int ), typeof( string ) ),
+                new TypeMappingKey( typeof( string ), typeof( int ) ),
+                new TypeMappingKey( typeof( string ), typeof( Guid ) )
+            };
+
+            var result = sut.Build();
+
+            using ( new AssertionScope() )
+            {
+                result.Should().BeOfType<TypeMapper>();
+                result.GetConfiguredMappings().Should().BeEquivalentTo( expectedKeys );
+            }
+        }
+    }
+}
