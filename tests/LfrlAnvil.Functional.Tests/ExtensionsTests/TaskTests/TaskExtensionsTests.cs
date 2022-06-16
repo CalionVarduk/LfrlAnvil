2f4ef7329ea59abdbf812de4867fe8b411054302
Łukasz Furlepa <lucas.furlepa@gmail.com>
2022-06-16 12:12:25 +0200

    Functional: - Add ToNil & IgnoreResult extension methods for Task & ValueTask;

---
+++ tests/LfrlAnvil.Functional.Tests/ExtensionsTests/TaskTests/TaskExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using LfrlAnvil.Functional.Extensions;
+using LfrlAnvil.TestExtensions;
+using LfrlAnvil.TestExtensions.FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace LfrlAnvil.Functional.Tests.ExtensionsTests.TaskTests
+{
+    public class TaskExtensionsTests : TestsBase
+    {
+        [Fact]
+        public async Task ToNil_ShouldAwaitProvidedTask()
+        {
+            var action = Substitute.For<Action>();
+
+            var result = await Task.Run( action ).ToNil();
+
+            using ( new AssertionScope() )
+            {
+                result.Should().Be( Nil.Instance );
+                action.Verify().CallCount.Should().Be( 1 );
+            }
+        }
+
+        [Fact]
+        public async Task ToNil_ForValueTask_ShouldAwaitProvidedTask()
+        {
+            var action = Substitute.For<Action>();
+
+            var result = await new ValueTask( Task.Run( action ) ).ToNil();
+
+            using ( new AssertionScope() )
+            {
+                result.Should().Be( Nil.Instance );
+                action.Verify().CallCount.Should().Be( 1 );
+            }
+        }
+
+        [Fact]
+        public async Task IgnoreResult_ShouldAwaitProvidedTask()
+        {
+            var func = Substitute.For<Func<int>>();
+            await Task.Run( func ).IgnoreResult();
+            func.Verify().CallCount.Should().Be( 1 );
+        }
+
+        [Fact]
+        public async Task IgnoreResult_ForValueTask_ShouldAwaitProvidedTask()
+        {
+            var func = Substitute.For<Func<int>>();
+            await new ValueTask<int>( Task.Run( func ) ).IgnoreResult();
+            func.Verify().CallCount.Should().Be( 1 );
+        }
+    }
+}
