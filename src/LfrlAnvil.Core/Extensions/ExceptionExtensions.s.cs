35cea7020bf2c3d085f635baa7a9be2cd3bd2642
Łukasz Furlepa <lucas.furlepa@gmail.com>
2022-07-30 19:48:38 +0200

    Core: - Add Exception.Rethrow extension method;

---
+++ src/LfrlAnvil.Core/Extensions/ExceptionExtensions.s.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.ExceptionServices;
+
+namespace LfrlAnvil.Extensions;
+
+public static class ExceptionExtensions
+{
+    [DoesNotReturn]
+    [StackTraceHidden]
+    public static void Rethrow(this Exception exception)
+    {
+        ExceptionDispatchInfo.Throw( exception );
+    }
+}
---
+++ tests/LfrlAnvil.Core.Tests/ExtensionsTests/ExceptionTests/ExceptionExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using LfrlAnvil.Extensions;
+using LfrlAnvil.Functional;
+using LfrlAnvil.TestExtensions;
+using Xunit;
+
+namespace LfrlAnvil.Tests.ExtensionsTests.ExceptionTests;
+
+public class ExceptionExtensionsTests : TestsBase
+{
+    [Fact]
+    public void Rethrow_ShouldThrowExceptionWithOriginalStackTrace()
+    {
+        var exception = Unsafe.Try( () => throw new Exception() ).GetError();
+        var originalStackTrace = exception.StackTrace!.Split( Environment.NewLine );
+
+        var result = Unsafe.Try( () => exception.Rethrow() );
+
+        using ( new AssertionScope() )
+        {
+            result.HasError.Should().BeTrue();
+            result.GetError().Should().BeSameAs( exception );
+            result.GetError().StackTrace!.Split( Environment.NewLine ).Should().ContainInOrder( originalStackTrace );
+        }
+    }
+}
