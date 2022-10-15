89a2374faa3ef67b2134a0a67acacad88c878977
Łukasz Furlepa <lucas.furlepa@gmail.com>
2022-10-15 11:21:52 +0200

    Core: - Add Numerics.MathUtils static class;

---
+++ src/LfrlAnvil.Core/Numerics/MathUtils.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using LfrlAnvil.Exceptions;
+
+namespace LfrlAnvil.Numerics;
+
+public static class MathUtils
+{
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static ulong ToUnsigned(long value, ref int sign)
+    {
+        if ( value < 0 )
+        {
+            sign = -sign;
+            value = unchecked( -value );
+        }
+
+        return unchecked( (ulong)value );
+    }
+
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static long ToSigned(ulong value, int sign)
+    {
+        if ( sign >= 0 )
+            return checked( (long)value );
+
+        if ( value > (ulong)long.MaxValue + 1 )
+            ExceptionThrower.Throw( new OverflowException() );
+
+        return unchecked( -(long)value );
+    }
+
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static (ulong High, ulong Low) BigMulU128(ulong left, ulong right)
+    {
+        var high = Math.BigMul( left, right, out var low );
+        return (high, low);
+    }
+
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static (ulong High, ulong Low, int Sign) BigMul128(long left, long right)
+    {
+        var sign = 1;
+        var l = ToUnsigned( left, ref sign );
+        var r = ToUnsigned( right, ref sign );
+        var high = Math.BigMul( l, r, out var low );
+        return (high, low, sign);
+    }
+
+    [Pure]
+    public static (ulong QuotientHigh, ulong QuotientLow, ulong Remainder) BigDivU128(ulong leftHigh, ulong leftLow, uint right)
+    {
+        if ( right == 0 )
+            ExceptionThrower.Throw( new DivideByZeroException( ExceptionResources.DividedByZero ) );
+
+        var left0 = unchecked( (uint)leftLow );
+        var left1 = unchecked( (uint)(leftLow >> 32) );
+        var left2 = unchecked( (uint)leftHigh );
+        var left3 = unchecked( (uint)(leftHigh >> 32) );
+
+        var shift = BitOperations.LeadingZeroCount( right );
+        var backShift = 32 - shift;
+        var div = right << shift;
+
+        var val = ((ulong)left3 << shift) | ((ulong)left2 >> backShift);
+        var digit = Math.Min( val / div, uint.MaxValue );
+        while ( div * digit > val )
+            --digit;
+
+        left3 = GetFixedDividendDigit( left3, right, 0, ref digit );
+        var quotientHigh = digit << 32;
+
+        val = ((((ulong)left3 << 32) | left2) << shift) | ((ulong)left1 >> backShift);
+        digit = Math.Min( val / div, uint.MaxValue );
+        while ( div * digit > val )
+            --digit;
+
+        left2 = GetFixedDividendDigit( left2, right, left3, ref digit );
+        quotientHigh |= unchecked( (uint)digit );
+
+        val = ((((ulong)left2 << 32) | left1) << shift) | ((ulong)left0 >> backShift);
+        digit = Math.Min( val / div, uint.MaxValue );
+        while ( div * digit > val )
+            --digit;
+
+        left1 = GetFixedDividendDigit( left1, right, left2, ref digit );
+        var quotientLow = digit << 32;
+
+        val = (((ulong)left1 << 32) | left0) << shift;
+        digit = Math.Min( val / div, uint.MaxValue );
+        while ( div * digit > val )
+            --digit;
+
+        left0 = GetFixedDividendDigit( left0, right, left1, ref digit );
+        quotientLow |= unchecked( (uint)digit );
+
+        return (quotientHigh, quotientLow, left0);
+
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        static uint GetFixedDividendDigit(uint left, uint right, uint prevLeft, ref ulong digit)
+        {
+            if ( digit == 0 )
+                return left;
+
+            var carry = right * digit;
+            var subDigit = unchecked( (uint)carry );
+            carry >>= 32;
+            if ( left < subDigit )
+                ++carry;
+
+            left = unchecked( left - subDigit );
+            carry = unchecked( (uint)carry );
+
+            if ( carry == prevLeft )
+                return left;
+
+            Assume.Equals( carry, prevLeft + 1, nameof( carry ) );
+
+            var addDigit = (ulong)left + right;
+            left = unchecked( (uint)addDigit );
+            carry = unchecked( (uint)(addDigit >> 32) );
+
+            Assume.Equals( carry, 1U, nameof( carry ) );
+
+            --digit;
+            return left;
+        }
+    }
+
+    [Pure]
+    public static (ulong QuotientHigh, ulong QuotientLow, ulong Remainder) BigDivU128(ulong leftHigh, ulong leftLow, ulong right)
+    {
+        var right1 = unchecked( (uint)(right >> 32) );
+
+        if ( right1 == 0 )
+            return BigDivU128( leftHigh, leftLow, unchecked( (uint)right ) );
+
+        var right0 = unchecked( (uint)right );
+
+        var left0 = unchecked( (uint)leftLow );
+        var left1 = unchecked( (uint)(leftLow >> 32) );
+        var left2 = unchecked( (uint)leftHigh );
+        var left3 = unchecked( (uint)(leftHigh >> 32) );
+
+        var shift = BitOperations.LeadingZeroCount( right1 );
+        var backShift = 32 - shift;
+
+        var divHigh = (right1 << shift) | unchecked( (uint)((ulong)right0 >> backShift) );
+        var divLow = right0 << shift;
+
+        var valHigh = ((ulong)left3 << shift) | ((ulong)left2 >> backShift);
+        var valLow = (left2 << shift) | unchecked( (uint)((ulong)left1 >> backShift) );
+        var digit = Math.Min( valHigh / divHigh, uint.MaxValue );
+        while ( IsQuotientDigitTooBig( digit, valHigh, valLow, divHigh, divLow ) )
+            --digit;
+
+        FixDividendDigits( ref left2, ref left3, right0, right1, 0, ref digit );
+        var quotientHigh = (ulong)unchecked( (uint)digit );
+
+        valHigh = ((((ulong)left3 << 32) | left2) << shift) | ((ulong)left1 >> backShift);
+        valLow = (left1 << shift) | unchecked( (uint)((ulong)left0 >> backShift) );
+        digit = Math.Min( valHigh / divHigh, uint.MaxValue );
+        while ( IsQuotientDigitTooBig( digit, valHigh, valLow, divHigh, divLow ) )
+            --digit;
+
+        FixDividendDigits( ref left1, ref left2, right0, right1, left3, ref digit );
+        var quotientLow = digit << 32;
+
+        valHigh = ((((ulong)left2 << 32) | left1) << shift) | ((ulong)left0 >> backShift);
+        valLow = left0 << shift;
+        digit = Math.Min( valHigh / divHigh, uint.MaxValue );
+        while ( IsQuotientDigitTooBig( digit, valHigh, valLow, divHigh, divLow ) )
+            --digit;
+
+        FixDividendDigits( ref left0, ref left1, right0, right1, left2, ref digit );
+        quotientLow |= unchecked( (uint)digit );
+
+        var remainder = ((ulong)left1 << 32) | left0;
+        return (quotientHigh, quotientLow, remainder);
+
+        [Pure]
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        static bool IsQuotientDigitTooBig(ulong digit, ulong valHigh, uint valLow, uint divHigh, uint divLow)
+        {
+            Assume.IsLessThanOrEqualTo( digit, uint.MaxValue, nameof( digit ) );
+
+            var checkHigh = divHigh * digit;
+            var checkLow = divLow * digit;
+
+            checkHigh += checkLow >> 32;
+            checkLow &= uint.MaxValue;
+
+            if ( checkHigh < valHigh )
+                return false;
+
+            return checkHigh > valHigh || checkLow > valLow;
+        }
+
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        static void FixDividendDigits(ref uint left0, ref uint left1, uint right0, uint right1, uint prevLeft, ref ulong digit)
+        {
+            if ( digit == 0 )
+                return;
+
+            var carry = right0 * digit;
+            var subDigit = unchecked( (uint)carry );
+            carry >>= 32;
+            if ( left0 < subDigit )
+                ++carry;
+
+            left0 = unchecked( left0 - subDigit );
+
+            carry += right1 * digit;
+            subDigit = unchecked( (uint)carry );
+            carry >>= 32;
+            if ( left1 < subDigit )
+                ++carry;
+
+            left1 = unchecked( left1 - subDigit );
+            carry = unchecked( (uint)carry );
+
+            if ( carry == prevLeft )
+                return;
+
+            Assume.Equals( carry, prevLeft + 1, nameof( carry ) );
+
+            var addDigit = (ulong)left0 + right0;
+            left0 = unchecked( (uint)addDigit );
+
+            addDigit = left1 + (addDigit >> 32) + right1;
+            left1 = unchecked( (uint)addDigit );
+            carry = unchecked( (uint)(addDigit >> 32) );
+
+            Assume.Equals( carry, 1U, nameof( carry ) );
+            --digit;
+        }
+    }
+}
---
+++ tests/LfrlAnvil.Core.Tests/NumericsTests/MathUtilsTests/MathUtilsTests.cs
@@ -0,0 +1,164 @@
+﻿using FluentAssertions.Execution;
+using LfrlAnvil.Functional;
+using LfrlAnvil.Numerics;
+
+namespace LfrlAnvil.Tests.NumericsTests.MathUtilsTests;
+
+public class MathUtilsTests : TestsBase
+{
+    [Theory]
+    [InlineData( 0 )]
+    [InlineData( 1 )]
+    [InlineData( uint.MaxValue )]
+    [InlineData( long.MaxValue )]
+    public void ToUnsigned_ShouldReturnCorrectResult_WhenValueIsNotNegative(long value)
+    {
+        var sign = Fixture.Create<int>();
+        var refSign = sign;
+        var result = MathUtils.ToUnsigned( value, ref refSign );
+
+        using ( new AssertionScope() )
+        {
+            result.Should().Be( (ulong)value );
+            refSign.Should().Be( sign );
+        }
+    }
+
+    [Theory]
+    [InlineData( -1, 1 )]
+    [InlineData( int.MinValue, (ulong)int.MaxValue + 1 )]
+    [InlineData( long.MinValue, (ulong)long.MaxValue + 1 )]
+    public void ToUnsigned_ShouldReturnCorrectResult_WhenValueIsNegative(long value, ulong expected)
+    {
+        var sign = Fixture.Create<int>();
+        var refSign = sign;
+        var result = MathUtils.ToUnsigned( value, ref refSign );
+
+        using ( new AssertionScope() )
+        {
+            result.Should().Be( expected );
+            refSign.Should().Be( -sign );
+        }
+    }
+
+    [Theory]
+    [InlineData( 0, 0 )]
+    [InlineData( 1, 1 )]
+    [InlineData( uint.MaxValue, uint.MaxValue )]
+    [InlineData( long.MaxValue, long.MaxValue )]
+    public void ToSigned_ShouldReturnCorrectResult_WhenValueIsNotNegative(ulong value, long expected)
+    {
+        var result = MathUtils.ToSigned( value, sign: 1 );
+        result.Should().Be( expected );
+    }
+
+    [Theory]
+    [InlineData( 1, -1 )]
+    [InlineData( uint.MaxValue, -uint.MaxValue )]
+    [InlineData( long.MaxValue, -long.MaxValue )]
+    [InlineData( (ulong)long.MaxValue + 1, long.MinValue )]
+    public void ToSigned_ShouldReturnCorrectResult_WhenValueIsNegative(ulong value, long expected)
+    {
+        var result = MathUtils.ToSigned( value, sign: -1 );
+        result.Should().Be( expected );
+    }
+
+    [Fact]
+    public void ToSigned_ShouldThrowOverflowException_WhenValueIsNotNegativeAndExceedsInt64MaxValue()
+    {
+        var action = Lambda.Of( () => MathUtils.ToSigned( (ulong)long.MaxValue + 1, sign: 1 ) );
+        action.Should().ThrowExactly<OverflowException>();
+    }
+
+    [Fact]
+    public void ToSigned_ShouldThrowOverflowException_WhenValueIsNegativeAndExceedsInt64MinValue()
+    {
+        var action = Lambda.Of( () => MathUtils.ToSigned( (ulong)long.MaxValue + 2, sign: -1 ) );
+        action.Should().ThrowExactly<OverflowException>();
+    }
+
+    [Theory]
+    [InlineData( 0, 0, 0, 0 )]
+    [InlineData( 100, 200, 0, 20000 )]
+    [InlineData( long.MaxValue, 1, 0, (ulong)long.MaxValue )]
+    [InlineData( 1, long.MaxValue, 0, (ulong)long.MaxValue )]
+    [InlineData( 3, 6148914691236517205, 0, ulong.MaxValue )]
+    [InlineData( 1L << 62, 4, 1, 0 )]
+    [InlineData( long.MaxValue, long.MaxValue, 4611686018427387903UL, 1 )]
+    public void BigMulU128_ShouldReturnCorrectResult(ulong a, ulong b, ulong expectedHigh, ulong expectedLow)
+    {
+        var (high, low) = MathUtils.BigMulU128( a, b );
+
+        using ( new AssertionScope() )
+        {
+            high.Should().Be( expectedHigh );
+            low.Should().Be( expectedLow );
+        }
+    }
+
+    [Theory]
+    [InlineData( 0, 0, 0, 0, 1 )]
+    [InlineData( 100, 200, 0, 20000, 1 )]
+    [InlineData( -100, 200, 0, 20000, -1 )]
+    [InlineData( 100, -200, 0, 20000, -1 )]
+    [InlineData( -100, -200, 0, 20000, 1 )]
+    [InlineData( long.MaxValue, 1, 0, (ulong)long.MaxValue, 1 )]
+    [InlineData( long.MinValue, 1, 0, (ulong)long.MaxValue + 1, -1 )]
+    [InlineData( -1, long.MaxValue, 0, (ulong)long.MaxValue, -1 )]
+    [InlineData( -1, long.MinValue, 0, (ulong)long.MaxValue + 1, 1 )]
+    [InlineData( 3, 6148914691236517205, 0, ulong.MaxValue, 1 )]
+    [InlineData( 1L << 62, 4, 1, 0, 1 )]
+    [InlineData( long.MaxValue, long.MaxValue, 4611686018427387903UL, 1, 1 )]
+    [InlineData( long.MinValue, long.MinValue, 4611686018427387904UL, 0, 1 )]
+    public void BigMul128_ShouldReturnCorrectResult(long a, long b, ulong expectedHigh, ulong expectedLow, int expectedSign)
+    {
+        var (high, low, sign) = MathUtils.BigMul128( a, b );
+
+        using ( new AssertionScope() )
+        {
+            high.Should().Be( expectedHigh );
+            low.Should().Be( expectedLow );
+            sign.Should().Be( expectedSign );
+        }
+    }
+
+    [Theory]
+    [InlineData( 0, 0, 1, 0, 0, 0 )]
+    [InlineData( 0, 0, ulong.MaxValue, 0, 0, 0 )]
+    [InlineData( 0, 6, 2, 0, 3, 0 )]
+    [InlineData( 0, 7, 3, 0, 2, 1 )]
+    [InlineData( 0, 123456789054321, ulong.MaxValue, 0, 0, 123456789054321 )]
+    [InlineData( ulong.MaxValue, ulong.MaxValue, 1, ulong.MaxValue, ulong.MaxValue, 0 )]
+    [InlineData( 64, 123456789, 456321, 0, 2587195462662324, 410209 )]
+    [InlineData( 4241943008571542805, 1432135365720863893, 1481763723909, 2862766, 2486493130135359103, 639595366042 )]
+    [InlineData( 3833045038591339946, 573693638711787792, 6350190974633306954, 0, 11134657387840150926, 701016773156101124 )]
+    [InlineData( 4605921407466066877, 7887564637810862666, 4648446336110425948, 0, 18277989522460577627, 2421975627461220502 )]
+    [InlineData( 5304349795661473109, 2867999000909509222, 219012612294601820, 24, 4046856291975950627, 213928629415675346 )]
+    [InlineData( 829678622093003025, 1975874835207407424, 4555612114624923477, 0, 3359563724937039266, 3558444750694797942 )]
+    [InlineData( 0, 6149394939174668089, 2208266555358409424, 0, 2, 1732861828457849241 )]
+    [InlineData( 123456789, 9876543210, 1UL << 63, 0, 246913578, 9876543210 )]
+    public void BigDivU128_ShouldReturnCorrectResult(
+        ulong aHigh,
+        ulong aLow,
+        ulong b,
+        ulong expectedHigh,
+        ulong expectedLow,
+        ulong expectedRemainder)
+    {
+        var (high, low, remainder) = MathUtils.BigDivU128( aHigh, aLow, b );
+
+        using ( new AssertionScope() )
+        {
+            high.Should().Be( expectedHigh );
+            low.Should().Be( expectedLow );
+            remainder.Should().Be( expectedRemainder );
+        }
+    }
+
+    [Fact]
+    public void BigDivU128_ShouldThrowDivideByZeroException_WhenRightIsEqualToZero()
+    {
+        var action = Lambda.Of( () => MathUtils.BigDivU128( Fixture.Create<ulong>(), Fixture.Create<ulong>(), 0 ) );
+        action.Should().ThrowExactly<DivideByZeroException>();
+    }
+}
