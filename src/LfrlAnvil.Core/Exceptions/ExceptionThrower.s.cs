e9fd0c426a2e7c5620010031f06085a514289047
Łukasz Furlepa <lucas.furlepa@gmail.com>
2023-05-06 12:34:14 +0200

    Core: - Add StackTraceHidden attribute to ExceptionThrower.Throw method;

--- src/LfrlAnvil.Core/Exceptions/ExceptionThrower.s.cs
+++ src/LfrlAnvil.Core/Exceptions/ExceptionThrower.s.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 
 namespace LfrlAnvil.Exceptions;
@@ -6,6 +7,7 @@ namespace LfrlAnvil.Exceptions;
 public static class ExceptionThrower
 {
     [DoesNotReturn]
+    [StackTraceHidden]
     public static void Throw(Exception exception)
     {
         throw exception;
