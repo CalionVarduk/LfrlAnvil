26df8ff4fa755e580b7fce5a3b399dba80626594
Łukasz Furlepa <lucas.furlepa@gmail.com>
2023-10-10 20:02:13 +0200

    Core: - Add MemoryExtensions with GetEnumerator methods;

---
+++ src/LfrlAnvil.Core/Extensions/MemoryExtensions.s.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace LfrlAnvil.Extensions;
+
+public static class MemoryExtensions
+{
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static ReadOnlySpan<T>.Enumerator GetEnumerator<T>(this ReadOnlyMemory<T> source)
+    {
+        return source.Span.GetEnumerator();
+    }
+
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static Span<T>.Enumerator GetEnumerator<T>(this Memory<T> source)
+    {
+        return source.Span.GetEnumerator();
+    }
+}
---
+++ tests/LfrlAnvil.Core.Tests/ExtensionsTests/MemoryTests/MemoryExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using LfrlAnvil.Extensions;
+using LfrlAnvil.TestExtensions.FluentAssertions;
+
+namespace LfrlAnvil.Tests.ExtensionsTests.MemoryTests;
+
+public class MemoryExtensionsTests : TestsBase
+{
+    [Fact]
+    public void GetEnumerator_ForReadOnlyMemory_ShouldBeEquivalentToSpanEnumerator()
+    {
+        ReadOnlyMemory<int> sut = new[] { 1, 2, 3 }.AsMemory();
+
+        var result = new List<int>();
+        foreach ( var e in sut )
+            result.Add( e );
+
+        result.Should().BeSequentiallyEqualTo( 1, 2, 3 );
+    }
+
+    [Fact]
+    public void GetEnumerator_ForMemory_ShouldBeEquivalentToSpanEnumerator()
+    {
+        var sut = new[] { 1, 2, 3 }.AsMemory();
+
+        var result = new List<int>();
+        foreach ( var e in sut )
+            result.Add( e );
+
+        result.Should().BeSequentiallyEqualTo( 1, 2, 3 );
+    }
+}
