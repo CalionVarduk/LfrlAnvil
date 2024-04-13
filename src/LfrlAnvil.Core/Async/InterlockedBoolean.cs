9fc2561a67824661f8f6efde08f994af6cfebea5
Łukasz Furlepa <lucas.furlepa@gmail.com>
2024-04-13 20:16:12 +0200

    Core: - Add InterlockedBoolean struct;

---
+++ src/LfrlAnvil.Core/Async/InterlockedBoolean.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using LfrlAnvil.Extensions;
+
+namespace LfrlAnvil.Async;
+
+public struct InterlockedBoolean : IEquatable<InterlockedBoolean>, IComparable<InterlockedBoolean>, IComparable
+{
+    private int _value;
+
+    public InterlockedBoolean(bool value)
+    {
+        _value = value ? 1 : 0;
+    }
+
+    public bool Value => Interlocked.Add( ref _value, 0 ).IsOdd();
+
+    [Pure]
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+
+    [Pure]
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
+
+    [Pure]
+    public override bool Equals(object? obj)
+    {
+        return obj is InterlockedBoolean b && Equals( b );
+    }
+
+    [Pure]
+    public int CompareTo(object? obj)
+    {
+        return obj is InterlockedBoolean b ? CompareTo( b ) : 1;
+    }
+
+    [Pure]
+    public bool Equals(InterlockedBoolean other)
+    {
+        return Value == other.Value;
+    }
+
+    [Pure]
+    public int CompareTo(InterlockedBoolean other)
+    {
+        return Value.CompareTo( other.Value );
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public bool WriteTrue()
+    {
+        return Interlocked.Exchange( ref _value, 1 ).IsEven();
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public bool WriteFalse()
+    {
+        return Interlocked.Exchange( ref _value, 0 ).IsOdd();
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public bool Toggle()
+    {
+        return Interlocked.Increment( ref _value ).IsOdd();
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static bool operator ==(InterlockedBoolean a, InterlockedBoolean b)
+    {
+        return a.Equals( b );
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static bool operator !=(InterlockedBoolean a, InterlockedBoolean b)
+    {
+        return ! a.Equals( b );
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static bool operator >=(InterlockedBoolean a, InterlockedBoolean b)
+    {
+        return a.CompareTo( b ) >= 0;
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static bool operator <=(InterlockedBoolean a, InterlockedBoolean b)
+    {
+        return a.CompareTo( b ) <= 0;
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static bool operator >(InterlockedBoolean a, InterlockedBoolean b)
+    {
+        return a.CompareTo( b ) > 0;
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static bool operator <(InterlockedBoolean a, InterlockedBoolean b)
+    {
+        return a.CompareTo( b ) < 0;
+    }
+}
---
+++ tests/LfrlAnvil.Core.Tests/AsyncTests/InterlockedBooleanTests.cs
@@ -0,0 +1,203 @@
+﻿using System.Linq;
+using LfrlAnvil.Async;
+using LfrlAnvil.Extensions;
+
+namespace LfrlAnvil.Tests.AsyncTests;
+
+public class InterlockedBooleanTests : TestsBase
+{
+    [Fact]
+    public void Default_ShouldBeFalse()
+    {
+        var sut = default( InterlockedBoolean );
+        sut.Value.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData( true )]
+    [InlineData( false )]
+    public void Ctor_ShouldCreateWithCorrectValue(bool value)
+    {
+        var sut = new InterlockedBoolean( value );
+        sut.Value.Should().Be( value );
+    }
+
+    [Theory]
+    [InlineData( true, "True" )]
+    [InlineData( false, "False" )]
+    public void ToString_ShouldReturnCorrectResult(bool value, string expected)
+    {
+        var sut = new InterlockedBoolean( value );
+        var result = sut.ToString();
+        result.Should().Be( expected );
+    }
+
+    [Theory]
+    [InlineData( true )]
+    [InlineData( false )]
+    public void GetHashCode_ShouldReturnCorrectResult(bool value)
+    {
+        var a = new InterlockedBoolean( value );
+        var b = new InterlockedBoolean( value );
+        b.Toggle();
+        b.Toggle();
+        var expected = value.GetHashCode();
+
+        var result1 = a.GetHashCode();
+        var result2 = b.GetHashCode();
+
+        using ( new AssertionScope() )
+        {
+            result1.Should().Be( expected );
+            result2.Should().Be( expected );
+        }
+    }
+
+    [Theory]
+    [InlineData( false, true )]
+    [InlineData( true, false )]
+    public void WriteTrue_ShouldUpdateValueToTrueAndReturnTrueOnlyWhenValueWasChanged(bool value, bool expected)
+    {
+        var sut = new InterlockedBoolean( value );
+        var result = sut.WriteTrue();
+
+        using ( new AssertionScope() )
+        {
+            result.Should().Be( expected );
+            sut.Value.Should().BeTrue();
+        }
+    }
+
+    [Theory]
+    [InlineData( false, false )]
+    [InlineData( true, true )]
+    public void WriteFalse_ShouldUpdateValueToFalseAndReturnTrueOnlyWhenValueWasChanged(bool value, bool expected)
+    {
+        var sut = new InterlockedBoolean( value );
+        var result = sut.WriteFalse();
+
+        using ( new AssertionScope() )
+        {
+            result.Should().Be( expected );
+            sut.Value.Should().BeFalse();
+        }
+    }
+
+    [Theory]
+    [InlineData( true, false )]
+    [InlineData( false, true )]
+    public void Toggle_ShouldNegateValueAndReturnNewValue(bool value, bool expected)
+    {
+        var sut = new InterlockedBoolean( value );
+        var result = sut.Toggle();
+
+        using ( new AssertionScope() )
+        {
+            result.Should().Be( expected );
+            sut.Value.Should().Be( expected );
+        }
+    }
+
+    [Theory]
+    [InlineData( true )]
+    [InlineData( false )]
+    public void Toggle_CalledMultipleTimes_ShouldBehaveCorrectly(bool value)
+    {
+        var values = new bool[100];
+        var sut = new InterlockedBoolean( value );
+
+        for ( var i = 0; i < values.Length; ++i )
+            values[i] = sut.Toggle();
+
+        using ( new AssertionScope() )
+        {
+            values.Where( (_, i) => i.IsEven() ).Should().AllBeEquivalentTo( ! value );
+            values.Where( (_, i) => i.IsOdd() ).Should().AllBeEquivalentTo( value );
+            sut.Value.Should().Be( values[^1] );
+        }
+    }
+
+    [Theory]
+    [InlineData( true, true, true )]
+    [InlineData( true, false, false )]
+    [InlineData( false, true, false )]
+    public void EqualityOperator_ShouldReturnCorrectResult(bool a, bool b, bool expected)
+    {
+        var left = new InterlockedBoolean( a );
+        var right = new InterlockedBoolean( b );
+
+        var result = left == right;
+
+        result.Should().Be( expected );
+    }
+
+    [Theory]
+    [InlineData( true, true, false )]
+    [InlineData( true, false, true )]
+    [InlineData( false, true, true )]
+    public void InequalityOperator_ShouldReturnCorrectResult(bool a, bool b, bool expected)
+    {
+        var left = new InterlockedBoolean( a );
+        var right = new InterlockedBoolean( b );
+
+        var result = left != right;
+
+        result.Should().Be( expected );
+    }
+
+    [Theory]
+    [InlineData( true, true, true )]
+    [InlineData( true, false, true )]
+    [InlineData( false, true, false )]
+    public void GreaterThanOrEqualToOperator_ShouldReturnCorrectResult(bool a, bool b, bool expected)
+    {
+        var left = new InterlockedBoolean( a );
+        var right = new InterlockedBoolean( b );
+
+        var result = left >= right;
+
+        result.Should().Be( expected );
+    }
+
+    [Theory]
+    [InlineData( true, true, true )]
+    [InlineData( true, false, false )]
+    [InlineData( false, true, true )]
+    public void LessThanOrEqualToOperator_ShouldReturnCorrectResult(bool a, bool b, bool expected)
+    {
+        var left = new InterlockedBoolean( a );
+        var right = new InterlockedBoolean( b );
+
+        var result = left <= right;
+
+        result.Should().Be( expected );
+    }
+
+    [Theory]
+    [InlineData( true, true, false )]
+    [InlineData( true, false, true )]
+    [InlineData( false, true, false )]
+    public void GreaterThanOperator_ShouldReturnCorrectResult(bool a, bool b, bool expected)
+    {
+        var left = new InterlockedBoolean( a );
+        var right = new InterlockedBoolean( b );
+
+        var result = left > right;
+
+        result.Should().Be( expected );
+    }
+
+    [Theory]
+    [InlineData( true, true, false )]
+    [InlineData( true, false, false )]
+    [InlineData( false, true, true )]
+    public void LessThanOperator_ShouldReturnCorrectResult(bool a, bool b, bool expected)
+    {
+        var left = new InterlockedBoolean( a );
+        var right = new InterlockedBoolean( b );
+
+        var result = left < right;
+
+        result.Should().Be( expected );
+    }
+}
