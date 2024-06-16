f63d587069a945120937c40c3d22323b860b51bb
Łukasz Furlepa <lucas.furlepa@gmail.com>
2024-06-16 16:56:02 +0200

    Core: - Add SemaphoreEntrySlim struct;

---
+++ src/LfrlAnvil.Core/Async/SemaphoreEntrySlim.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LfrlAnvil.Async;
+
+/// <summary>
+/// A lightweight, disposable object representing an acquired semaphore entry.
+/// </summary>
+public readonly struct SemaphoreEntrySlim : IDisposable
+{
+    private readonly SemaphoreSlim? _semaphore;
+
+    private SemaphoreEntrySlim(SemaphoreSlim semaphore)
+    {
+        _semaphore = semaphore;
+    }
+
+    /// <summary>
+    /// Specifies whether or not a semaphore has been entered.
+    /// </summary>
+    public bool Entered => _semaphore is not null;
+
+    /// <summary>
+    /// Enters a <see cref="SemaphoreSlim"/> instance by blocking the current thread.
+    /// </summary>
+    /// <param name="semaphore">A semaphore object to enter.</param>
+    /// <returns>A disposable <see cref="SemaphoreEntrySlim"/> instance.</returns>
+    /// <exception cref="ObjectDisposedException">The <paramref name="semaphore"/> has been disposed.</exception>
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static SemaphoreEntrySlim Enter(SemaphoreSlim semaphore)
+    {
+        semaphore.Wait();
+        return new SemaphoreEntrySlim( semaphore );
+    }
+
+    /// <summary>
+    /// Attempts to enter a <see cref="SemaphoreSlim"/> instance by blocking the current thread.
+    /// If the <paramref name="semaphore"/> has been disposed, then it will not be entered.
+    /// </summary>
+    /// <param name="semaphore">A semaphore object to enter.</param>
+    /// <param name="entered">An <b>out</b> parameter set to <b>true</b> when semaphore has been entered, otherwise <b>false</b>.</param>
+    /// <returns>
+    /// A disposable <see cref="SemaphoreEntrySlim"/> instance.
+    /// Semaphore will not be entered if <paramref name="semaphore"/> has been disposed.
+    /// </returns>
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static SemaphoreEntrySlim TryEnter(SemaphoreSlim semaphore, out bool entered)
+    {
+        try
+        {
+            semaphore.Wait();
+        }
+        catch ( ObjectDisposedException )
+        {
+            entered = false;
+            return default;
+        }
+
+        entered = true;
+        return new SemaphoreEntrySlim( semaphore );
+    }
+
+    /// <summary>
+    /// Enters a <see cref="SemaphoreSlim"/> instance asynchronously.
+    /// </summary>
+    /// <param name="semaphore">A semaphore object to enter.</param>
+    /// <param name="cancellationToken">Optional <see cref="CancellationToken"/> to observe.</param>
+    /// <returns>A task that returns a disposable <see cref="SemaphoreEntrySlim"/> instance.</returns>
+    /// <exception cref="ObjectDisposedException">The <paramref name="semaphore"/> has been disposed.</exception>
+    /// <exception cref="OperationCanceledException"><paramref name="cancellationToken"/> was cancelled.</exception>
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static async ValueTask<SemaphoreEntrySlim> EnterAsync(SemaphoreSlim semaphore, CancellationToken cancellationToken = default)
+    {
+        await semaphore.WaitAsync( cancellationToken ).ConfigureAwait( false );
+        return new SemaphoreEntrySlim( semaphore );
+    }
+
+    /// <summary>
+    /// Attempts to enter a <see cref="SemaphoreSlim"/> instance asynchronously.
+    /// If the <paramref name="semaphore"/> has been disposed, then it will not be entered.
+    /// </summary>
+    /// <param name="semaphore">A semaphore object to enter.</param>
+    /// <param name="cancellationToken">Optional <see cref="CancellationToken"/> to observe.</param>
+    /// <returns>
+    /// A task that returns a disposable <see cref="SemaphoreEntrySlim"/> instance.
+    /// Semaphore will not be entered if <paramref name="semaphore"/> has been disposed.
+    /// </returns>
+    /// <exception cref="OperationCanceledException"><paramref name="cancellationToken"/> was cancelled.</exception>
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static async ValueTask<SemaphoreEntrySlim> TryEnterAsync(SemaphoreSlim semaphore, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await semaphore.WaitAsync( cancellationToken ).ConfigureAwait( false );
+        }
+        catch ( ObjectDisposedException )
+        {
+            return default;
+        }
+
+        return new SemaphoreEntrySlim( semaphore );
+    }
+
+    /// <inheritdoc />
+    /// <exception cref="SemaphoreFullException">
+    /// Attempt to release a semaphore has thrown an exception of this type.
+    /// See <see cref="SemaphoreSlim.Release()"/> for more information.
+    /// </exception>
+    /// <remarks>Releases previously acquired semaphore entry.</remarks>
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public void Dispose()
+    {
+        try
+        {
+            _semaphore?.Release();
+        }
+        catch ( ObjectDisposedException )
+        {
+            // NOTE: do nothing
+        }
+    }
+}
---
+++ tests/LfrlAnvil.Core.Tests/AsyncTests/SemaphoreEntrySlimTests.cs
@@ -0,0 +1,168 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using LfrlAnvil.Async;
+using LfrlAnvil.Functional;
+
+namespace LfrlAnvil.Tests.AsyncTests;
+
+public class SemaphoreEntrySlimTests : TestsBase
+{
+    [Fact]
+    public void Enter_ShouldEnterSemaphore()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 1 );
+        _ = SemaphoreEntrySlim.Enter( semaphore );
+        semaphore.CurrentCount.Should().Be( 0 );
+    }
+
+    [Fact]
+    public void Enter_ShouldThrow_WhenSemaphoreIsDisposed()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 1 );
+        semaphore.Dispose();
+
+        var action = Lambda.Of( () => SemaphoreEntrySlim.Enter( semaphore ) );
+
+        action.Should().Throw<ObjectDisposedException>();
+    }
+
+    [Fact]
+    public void TryEnter_ShouldEnterSemaphore()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 1 );
+        var result = SemaphoreEntrySlim.TryEnter( semaphore, out var entered );
+
+        using ( new AssertionScope() )
+        {
+            semaphore.CurrentCount.Should().Be( 0 );
+            result.Entered.Should().Be( entered );
+            entered.Should().BeTrue();
+        }
+    }
+
+    [Fact]
+    public void TryEnter_ShouldReturnDefault_WhenLockIsDisposed()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 1 );
+        semaphore.Dispose();
+
+        var result = SemaphoreEntrySlim.TryEnter( semaphore, out var entered );
+
+        using ( new AssertionScope() )
+        {
+            result.Should().Be( default( SemaphoreEntrySlim ) );
+            result.Entered.Should().Be( entered );
+            entered.Should().BeFalse();
+        }
+    }
+
+    [Fact]
+    public async Task EnterAsync_ShouldEnterSemaphore()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 1 );
+        _ = await SemaphoreEntrySlim.EnterAsync( semaphore );
+        semaphore.CurrentCount.Should().Be( 0 );
+    }
+
+    [Fact]
+    public async Task EnterAsync_ShouldThrow_WhenSemaphoreIsDisposed()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 1 );
+        semaphore.Dispose();
+
+        Exception? exception = null;
+        try
+        {
+            await SemaphoreEntrySlim.EnterAsync( semaphore );
+        }
+        catch ( Exception exc )
+        {
+            exception = exc;
+        }
+
+        exception.Should().BeOfType<ObjectDisposedException>();
+    }
+
+    [Fact]
+    public async Task TryEnterAsync_ShouldEnterSemaphore()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 1 );
+        var result = await SemaphoreEntrySlim.TryEnterAsync( semaphore );
+
+        using ( new AssertionScope() )
+        {
+            semaphore.CurrentCount.Should().Be( 0 );
+            result.Entered.Should().BeTrue();
+        }
+    }
+
+    [Fact]
+    public async Task TryEnterAsync_ShouldReturnDefault_WhenLockIsDisposed()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 1 );
+        semaphore.Dispose();
+
+        var result = await SemaphoreEntrySlim.TryEnterAsync( semaphore );
+
+        using ( new AssertionScope() )
+        {
+            result.Should().Be( default( SemaphoreEntrySlim ) );
+            result.Entered.Should().BeFalse();
+        }
+    }
+
+    [Fact]
+    public void Dispose_ShouldReleaseSemaphoreOnce()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 2 );
+        var sut = SemaphoreEntrySlim.Enter( semaphore );
+
+        sut.Dispose();
+
+        semaphore.CurrentCount.Should().Be( 2 );
+    }
+
+    [Fact]
+    public void Dispose_ShouldNotThrowForDefault()
+    {
+        var sut = default( SemaphoreEntrySlim );
+        var action = Lambda.Of( () => sut.Dispose() );
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public void Dispose_ShouldNotThrowForDisposedSemaphore()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 1 );
+        var sut = SemaphoreEntrySlim.Enter( semaphore );
+        semaphore.Dispose();
+
+        var action = Lambda.Of( () => sut.Dispose() );
+
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public void MultipleEntries_ShouldBehaveCorrectly()
+    {
+        var semaphore = new SemaphoreSlim( initialCount: 2 );
+
+        var first = SemaphoreEntrySlim.Enter( semaphore );
+        var firstCount = semaphore.CurrentCount;
+        var second = SemaphoreEntrySlim.Enter( semaphore );
+        var secondCount = semaphore.CurrentCount;
+
+        second.Dispose();
+        var countAfterFirstDisposal = semaphore.CurrentCount;
+        first.Dispose();
+        var countAfterSecondDisposal = semaphore.CurrentCount;
+
+        using ( new AssertionScope() )
+        {
+            firstCount.Should().Be( 1 );
+            secondCount.Should().Be( 0 );
+            countAfterFirstDisposal.Should().Be( 1 );
+            countAfterSecondDisposal.Should().Be( 2 );
+        }
+    }
+}
