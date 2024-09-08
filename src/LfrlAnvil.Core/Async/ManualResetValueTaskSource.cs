1c6a0f2cbe4459b5df20e7cfdcf04455445ac79e
Łukasz Furlepa <lucas.furlepa@gmail.com>
2024-09-08 20:11:30 +0200

    Core: - Add ManualResetValueTaskSource<TResult> class;

---
+++ src/LfrlAnvil.Core/Async/ManualResetValueTaskSource.cs
@@ -0,0 +1,125 @@
+﻿// Copyright 2024 Łukasz Furlepa
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+
+namespace LfrlAnvil.Async;
+
+/// <summary>
+/// Represents an implementation of an object that can be wrapped by a <see cref="ValueTask{T}"/>.
+/// </summary>
+/// <typeparam name="TResult">The type of the result produced by the <see cref="ManualResetValueTaskSource{TResult}"/>.</typeparam>
+public sealed class ManualResetValueTaskSource<TResult> : IValueTaskSource<TResult>
+{
+    private ManualResetValueTaskSourceCore<TResult> _core;
+
+    /// <summary>
+    /// Creates a new <see cref="ManualResetValueTaskSource{T}"/> instance.
+    /// </summary>
+    /// <param name="runContinuationsAsynchronously">
+    /// Specifies whether or not to force continuations to run asynchronously. Equal to <b>true</b> by default.
+    /// </param>
+    public ManualResetValueTaskSource(bool runContinuationsAsynchronously = true)
+    {
+        _core = new ManualResetValueTaskSourceCore<TResult> { RunContinuationsAsynchronously = runContinuationsAsynchronously };
+    }
+
+    /// <summary>
+    /// Specifies whether or not continuations are forced to run asynchronously.
+    /// </summary>
+    public bool RunContinuationsAsynchronously => _core.RunContinuationsAsynchronously;
+
+    /// <summary>
+    /// Specifies the status of this value task source's current operation.
+    /// </summary>
+    public ValueTaskSourceStatus Status => _core.GetStatus( _core.Version );
+
+    /// <inheritdoc />
+    [Pure]
+    public override string ToString()
+    {
+        return $"{nameof( _core.Version )}: {_core.Version}, {nameof( Status )}: {Status}";
+    }
+
+    /// <summary>
+    /// Completes this value task source's current operation with a successful result.
+    /// </summary>
+    /// <param name="result">Result of the operation.</param>
+    /// <exception cref="InvalidOperationException">When current operation has already been completed.</exception>
+    public void SetResult(TResult result)
+    {
+        _core.SetResult( result );
+    }
+
+    /// <summary>
+    /// Completes this value task source's current operation with an error.
+    /// </summary>
+    /// <param name="error">Exception of the operation.</param>
+    /// <exception cref="InvalidOperationException">When current operation has already been completed.</exception>
+    public void SetException(Exception error)
+    {
+        _core.SetException( error );
+    }
+
+    /// <summary>
+    /// Completes this value task source's current operation with a cancellation.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token associated with the operation.</param>
+    /// <exception cref="InvalidOperationException">When current operation has already been completed.</exception>
+    public void SetCancelled(CancellationToken cancellationToken)
+    {
+        SetException( new OperationCanceledException( cancellationToken ) );
+    }
+
+    /// <summary>
+    /// Resets this value task source in order to prepare it for the next operation.
+    /// </summary>
+    public void Reset()
+    {
+        _core.Reset();
+    }
+
+    /// <summary>
+    /// Creates a <see cref="ValueTask{T}"/> instance from this value task source.
+    /// </summary>
+    /// <returns>New <see cref="ValueTask{T}"/> instance.</returns>
+    /// <remarks>The returned value task will complete once this value task source is manually told to complete.</remarks>
+    public ValueTask<TResult> GetTask()
+    {
+        return new ValueTask<TResult>( this, _core.Version );
+    }
+
+    TResult IValueTaskSource<TResult>.GetResult(short token)
+    {
+        return _core.GetResult( token );
+    }
+
+    ValueTaskSourceStatus IValueTaskSource<TResult>.GetStatus(short token)
+    {
+        return _core.GetStatus( token );
+    }
+
+    void IValueTaskSource<TResult>.OnCompleted(
+        Action<object?> continuation,
+        object? state,
+        short token,
+        ValueTaskSourceOnCompletedFlags flags)
+    {
+        _core.OnCompleted( continuation, state, token, flags );
+    }
+}
---
+++ tests/LfrlAnvil.Core.Tests/AsyncTests/ManualResetValueTaskSourceTests.cs
@@ -0,0 +1,161 @@
+﻿// Copyright 2024 Łukasz Furlepa
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+using LfrlAnvil.Async;
+
+namespace LfrlAnvil.Tests.AsyncTests;
+
+public class ManualResetValueTaskSourceTests : TestsBase
+{
+    [Theory]
+    [InlineData( true )]
+    [InlineData( false )]
+    public void Ctor_ShouldCreateCorrectResult(bool runContinuationsAsynchronously)
+    {
+        var sut = new ManualResetValueTaskSource<int>( runContinuationsAsynchronously );
+
+        using ( new AssertionScope() )
+        {
+            sut.RunContinuationsAsynchronously.Should().Be( runContinuationsAsynchronously );
+            sut.Status.Should().Be( ValueTaskSourceStatus.Pending );
+        }
+    }
+
+    [Fact]
+    public async Task SetResult_ShouldCompleteCurrentOperation()
+    {
+        var sut = new ManualResetValueTaskSource<int>();
+
+        _ = Task.Run(
+            async () =>
+            {
+                await Task.Delay( 15 );
+                sut.SetResult( 123 );
+            } );
+
+        var result = await sut.GetTask();
+
+        using ( new AssertionScope() )
+        {
+            result.Should().Be( 123 );
+            sut.Status.Should().Be( ValueTaskSourceStatus.Succeeded );
+        }
+    }
+
+    [Fact]
+    public async Task SetException_ShouldCompleteCurrentOperation()
+    {
+        var exception = new Exception( "foo" );
+        var sut = new ManualResetValueTaskSource<int>();
+
+        _ = Task.Run(
+            async () =>
+            {
+                await Task.Delay( 15 );
+                sut.SetException( exception );
+            } );
+
+        Exception? caughtException = null;
+        try
+        {
+            _ = await sut.GetTask();
+        }
+        catch ( Exception exc )
+        {
+            caughtException = exc;
+        }
+
+        using ( new AssertionScope() )
+        {
+            caughtException.Should().BeSameAs( exception );
+            sut.Status.Should().Be( ValueTaskSourceStatus.Faulted );
+        }
+    }
+
+    [Fact]
+    public async Task SetCancelled_ShouldCompleteCurrentOperation()
+    {
+        var cancellationTokenSource = new CancellationTokenSource();
+        var token = cancellationTokenSource.Token;
+        cancellationTokenSource.Cancel();
+
+        var sut = new ManualResetValueTaskSource<int>();
+
+        _ = Task.Run(
+            async () =>
+            {
+                await Task.Delay( 15 );
+                sut.SetCancelled( token );
+            } );
+
+        Exception? caughtException = null;
+        try
+        {
+            _ = await sut.GetTask();
+        }
+        catch ( Exception exc )
+        {
+            caughtException = exc;
+        }
+
+        using ( new AssertionScope() )
+        {
+            caughtException.Should().BeOfType<OperationCanceledException>();
+            ((caughtException as OperationCanceledException)?.CancellationToken).Should().Be( token );
+            sut.Status.Should().Be( ValueTaskSourceStatus.Canceled );
+        }
+    }
+
+    [Fact]
+    public async Task Reset_ShouldStartNextOperation()
+    {
+        var sut = new ManualResetValueTaskSource<int>();
+
+        _ = Task.Run(
+            async () =>
+            {
+                await Task.Delay( 15 );
+                sut.SetResult( 123 );
+            } );
+
+        _ = await sut.GetTask();
+        sut.Reset();
+
+        _ = Task.Run(
+            async () =>
+            {
+                await Task.Delay( 15 );
+                sut.SetResult( 456 );
+            } );
+
+        var result = await sut.GetTask();
+
+        using ( new AssertionScope() )
+        {
+            result.Should().Be( 456 );
+            sut.Status.Should().Be( ValueTaskSourceStatus.Succeeded );
+        }
+    }
+
+    [Fact]
+    public void ToString_ShouldReturnInformationAboutVersionAndStatus()
+    {
+        var sut = new ManualResetValueTaskSource<int>();
+        var result = sut.ToString();
+        result.Should().Be( "Version: 0, Status: Pending" );
+    }
+}
