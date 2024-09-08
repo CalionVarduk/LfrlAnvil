0eb6e7fe4e38411c3ef504fb1e630b68cb7e0ef9
Łukasz Furlepa <lucas.furlepa@gmail.com>
2024-09-08 17:26:37 +0200

    Core: - Add Result & Result<T> structs;

---
+++ src/LfrlAnvil.Core/Result.cs
@@ -0,0 +1,147 @@
+﻿// Copyright 2024 Łukasz Furlepa
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using LfrlAnvil.Extensions;
+
+namespace LfrlAnvil;
+
+/// <summary>
+/// Represents a result of an operation.
+/// </summary>
+public readonly struct Result
+{
+    /// <summary>
+    /// Returns a valid <see cref="Result"/> instance.
+    /// </summary>
+    public static Result Valid => new Result( null );
+
+    internal Result(Exception? exception)
+    {
+        Exception = exception;
+    }
+
+    /// <summary>
+    /// Optional <see cref="System.Exception"/> associated with this result.
+    /// </summary>
+    public Exception? Exception { get; }
+
+    /// <summary>
+    /// Creates a <see cref="Result"/> with exception.
+    /// </summary>
+    /// <param name="exception"><see cref="System.Exception"/> associated with the result.</param>
+    /// <returns>New <see cref="Result"/> instance.</returns>
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static Result Error(Exception exception)
+    {
+        return new Result( exception );
+    }
+
+    /// <summary>
+    /// Creates a <see cref="Result{T}"/> with exception and optional value.
+    /// </summary>
+    /// <param name="exception"><see cref="System.Exception"/> associated with the result.</param>
+    /// <param name="value">Optional value associated with the result.</param>
+    /// <typeparam name="T">Type of an optional value.</typeparam>
+    /// <returns>New <see cref="Result{T}"/> instance.</returns>
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static Result<T> Error<T>(Exception exception, T? value = default)
+    {
+        return new Result<T>( value, exception );
+    }
+
+    /// <summary>
+    /// Creates a <see cref="Result{T}"/> with optional value.
+    /// </summary>
+    /// <param name="value">Optional value associated with the result.</param>
+    /// <typeparam name="T">Type of an optional value.</typeparam>
+    /// <returns>New <see cref="Result{T}"/> instance.</returns>
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static Result<T> Create<T>(T? value = default)
+    {
+        return new Result<T>( value, null );
+    }
+
+    /// <inheritdoc />
+    [Pure]
+    public override string ToString()
+    {
+        return Exception?.ToString() ?? "<VALID>";
+    }
+
+    /// <summary>
+    /// Rethrows the <see cref="Exception"/>, if it isn't null, otherwise does nothing.
+    /// </summary>
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public void ThrowIfError()
+    {
+        Exception?.Rethrow();
+    }
+}
+
+/// <summary>
+/// Represents a result of an operation.
+/// </summary>
+/// <typeparam name="T">Type of an optional value.</typeparam>
+public readonly struct Result<T>
+{
+    internal Result(T? value, Exception? exception)
+    {
+        Value = value;
+        Exception = exception;
+    }
+
+    /// <summary>
+    /// Optional value associated with this result.
+    /// </summary>
+    public T? Value { get; }
+
+    /// <summary>
+    /// Optional <see cref="System.Exception"/> associated with this result.
+    /// </summary>
+    public Exception? Exception { get; }
+
+    /// <inheritdoc />
+    [Pure]
+    public override string ToString()
+    {
+        return $"{nameof( Value )}: {Value?.ToString() ?? "<NULL>"}{Environment.NewLine}{Exception?.ToString() ?? "<VALID>"}";
+    }
+
+    /// <summary>
+    /// Rethrows the <see cref="Exception"/>, if it isn't null, otherwise does nothing.
+    /// </summary>
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public void ThrowIfError()
+    {
+        Exception?.Rethrow();
+    }
+
+    /// <summary>
+    /// Converts provided <paramref name="value"/> to <see cref="Result"/>.
+    /// </summary>
+    /// <param name="value">Object to convert.</param>
+    /// <returns>New <see cref="Result"/> instance.</returns>
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static implicit operator Result(Result<T> value)
+    {
+        return new Result( value.Exception );
+    }
+}
---
+++ tests/LfrlAnvil.Core.Tests/ResultTests/ResultTests.cs
@@ -0,0 +1,165 @@
+﻿// Copyright 2024 Łukasz Furlepa
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using LfrlAnvil.Functional;
+
+namespace LfrlAnvil.Tests.ResultTests;
+
+public class ResultTests : TestsBase
+{
+    [Fact]
+    public void Valid_ShouldReturnResultWithoutException()
+    {
+        var sut = Result.Valid;
+        sut.Exception.Should().BeNull();
+    }
+
+    [Fact]
+    public void Error_ShouldCreateResultWithException()
+    {
+        var exception = new Exception( "foo" );
+        var result = Result.Error( exception );
+        result.Exception.Should().BeSameAs( exception );
+    }
+
+    [Fact]
+    public void ToString_ShouldReturnCorrectResult_WhenExceptionIsNull()
+    {
+        var sut = Result.Valid;
+        var result = sut.ToString();
+        result.Should().Be( "<VALID>" );
+    }
+
+    [Fact]
+    public void ToString_ShouldReturnExceptionToString_WhenExceptionIsNotNull()
+    {
+        var exception = new Exception( "foo" );
+        var sut = Result.Error( exception );
+        var result = sut.ToString();
+        result.Should().Be( exception.ToString() );
+    }
+
+    [Fact]
+    public void ThrowIfError_ShouldDoNothing_WhenExceptionIsNull()
+    {
+        var sut = Result.Valid;
+        var action = Lambda.Of( () => sut.ThrowIfError() );
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public void ThrowIfError_ShouldThrow_WhenExceptionIsNotNull()
+    {
+        var exception = new Exception( "foo" );
+        var sut = Result.Error( exception );
+        var action = Lambda.Of( () => sut.ThrowIfError() );
+        action.Should().Throw<Exception>().And.Should().BeSameAs( exception );
+    }
+
+    [Fact]
+    public void Create_ShouldCreateGenericResultWithoutException()
+    {
+        var value = Fixture.Create<string>();
+        var result = Result.Create( value );
+
+        using ( new AssertionScope() )
+        {
+            result.Value.Should().BeSameAs( value );
+            result.Exception.Should().BeNull();
+        }
+    }
+
+    [Fact]
+    public void Error_ShouldCreateGenericResultWithException()
+    {
+        var value = Fixture.Create<string>();
+        var exception = new Exception( "foo" );
+        var result = Result.Error( exception, value );
+
+        using ( new AssertionScope() )
+        {
+            result.Value.Should().BeSameAs( value );
+            result.Exception.Should().BeSameAs( exception );
+        }
+    }
+
+    [Fact]
+    public void ToString_ShouldReturnCorrectResult_WhenGenericExceptionIsNull()
+    {
+        var value = Fixture.Create<string>();
+        var sut = Result.Create( value );
+        var result = sut.ToString();
+
+        result.Should()
+            .Be(
+                $"""
+                 Value: {value}
+                 <VALID>
+                 """ );
+    }
+
+    [Fact]
+    public void ToString_ShouldReturnExceptionToString_WhenGenericExceptionIsNotNull()
+    {
+        var value = Fixture.Create<string>();
+        var exception = new Exception( "foo" );
+        var sut = Result.Error( exception, value );
+        var result = sut.ToString();
+
+        result.Should()
+            .Be(
+                $"""
+                 Value: {value}
+                 {exception}
+                 """ );
+    }
+
+    [Fact]
+    public void ThrowIfError_ShouldDoNothing_WhenGenericExceptionIsNull()
+    {
+        var value = Fixture.Create<string>();
+        var sut = Result.Create( value );
+        var action = Lambda.Of( () => sut.ThrowIfError() );
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public void ThrowIfError_ShouldThrow_WhenGenericExceptionIsNotNull()
+    {
+        var value = Fixture.Create<string>();
+        var exception = new Exception( "foo" );
+        var sut = Result.Error( exception, value );
+        var action = Lambda.Of( () => sut.ThrowIfError() );
+        action.Should().Throw<Exception>().And.Should().BeSameAs( exception );
+    }
+
+    [Fact]
+    public void ResultConversionOperator_ShouldReturnValid_WhenExceptionIsNull()
+    {
+        var value = Fixture.Create<string>();
+        var sut = Result.Create( value );
+        var result = ( Result )sut;
+        result.Exception.Should().BeNull();
+    }
+
+    [Fact]
+    public void ResultConversionOperator_ShouldReturnError_WhenExceptionIsNotNull()
+    {
+        var value = Fixture.Create<string>();
+        var exception = new Exception( "foo" );
+        var sut = Result.Error( exception, value );
+        var result = ( Result )sut;
+        result.Exception.Should().BeSameAs( exception );
+    }
+}
