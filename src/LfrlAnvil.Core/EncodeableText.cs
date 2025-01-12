c8438672c6bfdeb983d1b1230addabad96eb6974
Łukasz Furlepa <lucas.furlepa@gmail.com>
2025-01-12 15:22:51 +0100

    Core: - Add EncodeableText struct;

---
+++ src/LfrlAnvil.Core/EncodeableText.cs
@@ -0,0 +1,123 @@
+﻿// Copyright 2025 Łukasz Furlepa
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LfrlAnvil;
+
+/// <summary>
+/// Allows to safely encode a set of characters into a span of bytes.
+/// </summary>
+public readonly struct EncodeableText
+{
+    private readonly Encoding? _encoding;
+
+    private EncodeableText(Encoding encoding, ReadOnlyMemory<char> value, int byteCount)
+    {
+        _encoding = encoding;
+        Value = value;
+        ByteCount = byteCount;
+    }
+
+    /// <summary>
+    /// Set of characters to encode into a span of bytes.
+    /// </summary>
+    public ReadOnlyMemory<char> Value { get; }
+
+    /// <summary>
+    /// Byte count of the <see cref="Value"/> to encode, according to the provided <see cref="Encoding"/>.
+    /// </summary>
+    public int ByteCount { get; }
+
+    /// <summary>
+    /// <see cref="System.Text.Encoding"/> used for encoding the <see cref="Value"/> into a span of bytes.
+    /// </summary>
+    public Encoding Encoding => _encoding ?? Encoding.UTF8;
+
+    /// <summary>
+    /// Attempts to create a new <see cref="EncodeableText"/> instance.
+    /// </summary>
+    /// <param name="encoding">
+    /// <see cref="System.Text.Encoding"/> used for encoding the <paramref name="value"/> into a span of bytes.
+    /// </param>
+    /// <param name="value"><see cref="System.String"/> to encode into a span of bytes.</param>
+    /// <returns>
+    /// New <see cref="EncodeableText"/> instance wrapped in a <see cref="Result{T}"/>
+    /// that specifies whether or not the operation was successful.
+    /// </returns>
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static Result<EncodeableText> Create(Encoding encoding, string value)
+    {
+        return Create( encoding, value.AsMemory() );
+    }
+
+    /// <summary>
+    /// Attempts to create a new <see cref="EncodeableText"/> instance.
+    /// </summary>
+    /// <param name="encoding">
+    /// <see cref="System.Text.Encoding"/> used for encoding the <paramref name="value"/> into a span of bytes.
+    /// </param>
+    /// <param name="value">Set of characters to encode into a span of bytes.</param>
+    /// <returns>
+    /// New <see cref="EncodeableText"/> instance wrapped in a <see cref="Result{T}"/>
+    /// that specifies whether or not the operation was successful.
+    /// </returns>
+    [Pure]
+    public static Result<EncodeableText> Create(Encoding encoding, ReadOnlyMemory<char> value)
+    {
+        try
+        {
+            var byteCount = encoding.GetByteCount( value.Span );
+            return Result.Create( new EncodeableText( encoding, value, byteCount ) );
+        }
+        catch ( Exception exc )
+        {
+            return Result.Error<EncodeableText>( exc );
+        }
+    }
+
+    /// <summary>
+    /// Returns a string representation of this <see cref="EncodeableText"/> instance.
+    /// </summary>
+    /// <returns>String representation.</returns>
+    [Pure]
+    public override string ToString()
+    {
+        return $"[Encoding: {Encoding.EncodingName}, ByteCount: {ByteCount}] '{Value}'";
+    }
+
+    /// <summary>
+    /// Attempts to encode the <see cref="Value"/> into the provided span of bytes.
+    /// </summary>
+    /// <param name="target">
+    /// Span of bytes to encode the <see cref="Value"/> into.
+    /// </param>
+    /// <returns><see cref="Result"/> instance that specifies whether or not the operation was successful.</returns>
+    public Result Encode(Span<byte> target)
+    {
+        try
+        {
+            Encoding.GetBytes( Value.Span, target );
+            return Result.Valid;
+        }
+        catch ( Exception exc )
+        {
+            return Result.Error( exc );
+        }
+    }
+}
---
+++ tests/LfrlAnvil.Core.Tests/EncodeableTextTests/EncodeableTextTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Text;
+using LfrlAnvil.TestExtensions.FluentAssertions;
+
+namespace LfrlAnvil.Tests.EncodeableTextTests;
+
+public class EncodeableTextTests : TestsBase
+{
+    [Fact]
+    public void Default_ShouldContainEmptyString()
+    {
+        var sut = default( EncodeableText );
+        using ( new AssertionScope() )
+        {
+            sut.ByteCount.Should().Be( 0 );
+            sut.Value.ToArray().Should().BeEmpty();
+            sut.Encoding.Should().BeSameAs( Encoding.UTF8 );
+        }
+    }
+
+    [Fact]
+    public void Create_ShouldReturnValidResult_WhenEncoderSuccessfullyRecoversByteCount()
+    {
+        var result = EncodeableText.Create( Encoding.Unicode, "foobar" );
+
+        using ( new AssertionScope() )
+        {
+            result.Exception.Should().BeNull();
+            result.Value.ByteCount.Should().Be( 12 );
+            result.Value.Value.ToString().Should().Be( "foobar" );
+            result.Value.Encoding.Should().BeSameAs( Encoding.Unicode );
+        }
+    }
+
+    [Fact]
+    public void Create_ShouldReturnInvalidResult_WhenEncoderFailsToRecoverByteCount()
+    {
+        var result = EncodeableText.Create( new InvalidEncoding(), "foobar" );
+        result.Exception.Should().BeOfType<NotSupportedException>();
+    }
+
+    [Fact]
+    public void ToString_ShouldReturnCorrectResult()
+    {
+        var sut = EncodeableText.Create( Encoding.UTF8, "foobar" ).Value;
+        var result = sut.ToString();
+        result.Should().Be( "[Encoding: Unicode (UTF-8), ByteCount: 6] 'foobar'" );
+    }
+
+    [Fact]
+    public void Encode_ShouldReturnValidResult_WhenEncoderPerformsOperationSuccessfully()
+    {
+        var target = new byte[10];
+        var sut = EncodeableText.Create( Encoding.Unicode, "foo" ).Value;
+
+        var result = sut.Encode( target );
+
+        using ( new AssertionScope() )
+        {
+            result.Exception.Should().BeNull();
+            target.Should().BeSequentiallyEqualTo<byte>( 102, 0, 111, 0, 111, 0, 0, 0, 0, 0 );
+        }
+    }
+
+    [Fact]
+    public void Encode_ShouldReturnInvalidResult_WhenEncoderFailsToEncode()
+    {
+        var target = new byte[4];
+        var sut = EncodeableText.Create( Encoding.Unicode, "foo" ).Value;
+
+        var result = sut.Encode( target );
+
+        result.Exception.Should().BeOfType<ArgumentException>();
+    }
+
+    private sealed class InvalidEncoding : Encoding
+    {
+        public override int GetByteCount(char[] chars, int index, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int GetCharCount(byte[] bytes, int index, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int GetChars(byte[] bytes, int byteIndex, int byteCount, char[] chars, int charIndex)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int GetMaxByteCount(int charCount)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int GetMaxCharCount(int byteCount)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
