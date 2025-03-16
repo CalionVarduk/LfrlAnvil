f9ca7afe453a8ffb0bb2363fbd324d791e81b6c0
Łukasz Furlepa <lucas.furlepa@gmail.com>
2025-03-16 08:51:27 +0100

    Chrono: - Add ValueTaskDelaySource class;

---
+++ src/LfrlAnvil.Chrono/Async/ValueTaskDelaySource.cs
@@ -0,0 +1,462 @@
+﻿// Copyright 2025 Łukasz Furlepa
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+using LfrlAnvil.Async;
+using LfrlAnvil.Extensions;
+using LfrlAnvil.Internal;
+
+namespace LfrlAnvil.Chrono.Async;
+
+/// <summary>
+/// Represents a source of <see cref="DelayValueTask"/> instances,
+/// which are value tasks that complete after a specified amount time has passed.
+/// </summary>
+public sealed class ValueTaskDelaySource : IDisposable, IAsyncDisposable
+{
+    private static Timestamp MaxTimestamp => new Timestamp( long.MaxValue );
+    private static Duration MaxScheduleDelay => Duration.FromMilliseconds( int.MaxValue );
+
+    private readonly ManualResetEventSlim _reset;
+    private readonly ITimestampProvider _timestamps;
+    private StackSlim<Node> _nodeCache;
+    private ListSlim<Node> _schedule;
+    private Task? _task;
+    private bool _isDisposed;
+
+    private ValueTaskDelaySource(ITimestampProvider timestamps)
+    {
+        _timestamps = timestamps;
+        _reset = new ManualResetEventSlim( false );
+        _nodeCache = StackSlim<Node>.Create();
+        _schedule = ListSlim<Node>.Create();
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="ValueTaskDelaySource"/> instance.
+    /// </summary>
+    /// <param name="timestamps"><see cref="Timestamp"/> provider.</param>
+    /// <returns>New <see cref="ValueTaskDelaySource"/> instance.</returns>
+    [Pure]
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static ValueTaskDelaySource Start(ITimestampProvider timestamps)
+    {
+        return Start( Task.Factory, timestamps );
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="ValueTaskDelaySource"/> instance.
+    /// </summary>
+    /// <param name="taskFactory"><see cref="TaskFactory"/> instance that will be used to start an underlying task.</param>
+    /// <param name="timestamps"><see cref="Timestamp"/> provider.</param>
+    /// <returns>New <see cref="ValueTaskDelaySource"/> instance.</returns>
+    [Pure]
+    public static ValueTaskDelaySource Start(TaskFactory taskFactory, ITimestampProvider timestamps)
+    {
+        var result = new ValueTaskDelaySource( timestamps );
+        var task = taskFactory.StartNew(
+            static o =>
+            {
+                Assume.IsNotNull( o );
+                var source = ReinterpretCast.To<ValueTaskDelaySource>( o );
+                try
+                {
+                    source.RunCore();
+                }
+                catch
+                {
+                    using ( source.AcquireLock() )
+                        source._task = null;
+
+                    source.Dispose();
+                }
+            },
+            result,
+            TaskCreationOptions.LongRunning );
+
+        using ( result.AcquireLock() )
+        {
+            if ( ! result._isDisposed )
+                result._task = task;
+        }
+
+        return result;
+    }
+
+    /// <inheritdoc/>
+    /// <remarks>All scheduled delays will be prematurely completed with <see cref="ValueTaskDelayResult.Disposed"/> result.</remarks>
+    public void Dispose()
+    {
+        DisposeAsync().AsTask().Wait();
+    }
+
+    /// <inheritdoc/>
+    /// <remarks>All scheduled delays will be prematurely completed with <see cref="ValueTaskDelayResult.Disposed"/> result.</remarks>
+    public async ValueTask DisposeAsync()
+    {
+        Task? task;
+        using ( AcquireLock() )
+        {
+            if ( _isDisposed )
+                return;
+
+            _isDisposed = true;
+            task = _task;
+            _task = null;
+
+            Clear();
+            _reset.Set();
+            _reset.TryDispose();
+        }
+
+        if ( task is not null )
+            await task.ConfigureAwait( false );
+    }
+
+    /// <summary>
+    /// Schedules a new delay task.
+    /// </summary>
+    /// <param name="delay">Amount of time after which the returned task completes.</param>
+    /// <param name="cancellationToken">Optional <see cref="CancellationToken"/> instance.</param>
+    /// <returns>New <see cref="DelayValueTask"/> instance.</returns>
+    public DelayValueTask Schedule(Duration delay, CancellationToken cancellationToken = default)
+    {
+        using ( AcquireLock() )
+        {
+            if ( _isDisposed )
+                return default;
+
+            if ( ! _nodeCache.TryPop( out var node ) )
+                node = new Node( this );
+
+            var now = _timestamps.GetNow();
+            node.Timestamp = now + delay;
+
+            if ( node.Timestamp <= now )
+            {
+                if ( cancellationToken.IsCancellationRequested )
+                    node.SetCancelled();
+                else
+                    node.SetCompleted();
+            }
+            else
+            {
+                node.HeapIndex = NullableIndex.Create( _schedule.Count );
+                _schedule.Add( node );
+                FixUp( node.HeapIndex.Value );
+                node.CancellationRegistration = cancellationToken.UnsafeRegister( Node.CancelUnsafe, node );
+
+                if ( node.HeapIndex.Value == 0 )
+                    _reset.Set();
+            }
+
+            return new DelayValueTask( node, node.GetTask() );
+        }
+    }
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    private ExclusiveLock AcquireLock()
+    {
+        return ExclusiveLock.SpinWaitEnter( _reset, spinWaitMultiplier: 4 );
+    }
+
+    [MethodImpl( MethodImplOptions.NoInlining )]
+    private void RunCore()
+    {
+        Duration delay;
+        using ( AcquireLock() )
+        {
+            if ( _isDisposed )
+                return;
+
+            delay = GetNextDelay();
+        }
+
+        while ( true )
+        {
+            try
+            {
+                _reset.Wait( delay );
+            }
+            catch ( ObjectDisposedException )
+            {
+                return;
+            }
+
+            using ( AcquireLock() )
+            {
+                if ( _isDisposed )
+                    return;
+
+                var now = _timestamps.GetNow();
+                while ( ! _schedule.IsEmpty )
+                {
+                    ref var first = ref _schedule.First();
+                    if ( first.Timestamp > now )
+                        break;
+
+                    var node = first;
+                    ref var last = ref Unsafe.Add( ref first, _schedule.Count - 1 );
+                    last.HeapIndex = NullableIndex.Create( 0 );
+                    first = last;
+                    _schedule.RemoveLast();
+                    FixDown( 0 );
+
+                    node.SetCompleted();
+                }
+
+                delay = GetNextDelay();
+            }
+        }
+    }
+
+    private Duration GetNextDelay()
+    {
+        var now = _timestamps.GetNow();
+        var next = MaxTimestamp;
+        if ( ! _schedule.IsEmpty )
+        {
+            var node = _schedule.First();
+            next = node.Timestamp;
+        }
+
+        var delay = next - now;
+        return delay.Clamp( Duration.Zero, MaxScheduleDelay );
+    }
+
+    private void Clear()
+    {
+        _nodeCache.Clear();
+        foreach ( var node in _schedule )
+            node.SetDisposed();
+
+        _schedule.Clear();
+    }
+
+    private void RemoveFromSchedule(int index)
+    {
+        var lastIndex = _schedule.Count - 1;
+        Assume.IsInRange( index, 0, lastIndex );
+
+        if ( index == lastIndex )
+        {
+            _schedule.RemoveLast();
+            return;
+        }
+
+        ref var first = ref _schedule.First();
+        ref var node = ref Unsafe.Add( ref first, index );
+        ref var last = ref Unsafe.Add( ref first, lastIndex );
+
+        var timestamp = node.Timestamp;
+        var lastTimestamp = last.Timestamp;
+        last.HeapIndex = node.HeapIndex;
+        node = last;
+        _schedule.RemoveLast();
+
+        if ( timestamp < lastTimestamp )
+            FixDown( index );
+        else
+            FixUp( index );
+    }
+
+    private void FixUp(int i)
+    {
+        var p = (i - 1) >> 1;
+        ref var first = ref _schedule.First();
+
+        while ( i > 0 )
+        {
+            ref var child = ref Unsafe.Add( ref first, i );
+            ref var parent = ref Unsafe.Add( ref first, p );
+            if ( child.Timestamp >= parent.Timestamp )
+                break;
+
+            (child.HeapIndex, parent.HeapIndex) = (parent.HeapIndex, child.HeapIndex);
+            (child, parent) = (parent, child);
+            i = p;
+            p = (p - 1) >> 1;
+        }
+    }
+
+    private void FixDown(int i)
+    {
+        var l = (i << 1) + 1;
+        ref var first = ref _schedule.First();
+
+        while ( l < _schedule.Count )
+        {
+            ref var parent = ref Unsafe.Add( ref first, i );
+            ref var child = ref Unsafe.Add( ref first, l );
+
+            var t = i;
+            ref var target = ref parent;
+            if ( child.Timestamp < target.Timestamp )
+            {
+                t = l;
+                target = ref child;
+            }
+
+            var r = l + 1;
+            if ( r < _schedule.Count )
+            {
+                child = ref Unsafe.Add( ref first, r )!;
+                if ( child.Timestamp < target.Timestamp )
+                {
+                    t = r;
+                    target = ref child;
+                }
+            }
+
+            if ( i == t )
+                break;
+
+            (target.HeapIndex, parent.HeapIndex) = (parent.HeapIndex, target.HeapIndex);
+            (target, parent) = (parent, target);
+            i = t;
+            l = (l << 1) + 1;
+        }
+    }
+
+    internal sealed class Node : IValueTaskSource<ValueTaskDelayResult>
+    {
+        internal readonly ValueTaskDelaySource Source;
+        internal Timestamp Timestamp;
+        internal NullableIndex HeapIndex;
+        internal CancellationTokenRegistration CancellationRegistration;
+        private ManualResetValueTaskSourceCore<ValueTaskDelayResult> _core;
+
+        internal Node(ValueTaskDelaySource source)
+        {
+            Source = source;
+            _core = new ManualResetValueTaskSourceCore<ValueTaskDelayResult> { RunContinuationsAsynchronously = true };
+            Timestamp = MaxTimestamp;
+            HeapIndex = NullableIndex.Null;
+        }
+
+        private ValueTaskSourceStatus Status => _core.GetStatus( _core.Version );
+
+        [Pure]
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        internal ValueTask<ValueTaskDelayResult> GetTask()
+        {
+            return new ValueTask<ValueTaskDelayResult>( this, _core.Version );
+        }
+
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        internal void SetCompleted()
+        {
+            if ( Status == ValueTaskSourceStatus.Pending )
+            {
+                CancellationRegistration.TryDispose();
+                CancellationRegistration = default;
+                Timestamp = MaxTimestamp;
+                HeapIndex = NullableIndex.Null;
+                _core.SetResult( ValueTaskDelayResult.Completed );
+            }
+        }
+
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        internal void SetDisposed()
+        {
+            if ( Status == ValueTaskSourceStatus.Pending )
+            {
+                CancellationRegistration.TryDispose();
+                CancellationRegistration = default;
+                Timestamp = MaxTimestamp;
+                HeapIndex = NullableIndex.Null;
+                _core.SetResult( ValueTaskDelayResult.Disposed );
+            }
+        }
+
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        internal void SetCancelled()
+        {
+            if ( Status == ValueTaskSourceStatus.Pending )
+            {
+                CancellationRegistration = default;
+                Timestamp = MaxTimestamp;
+                HeapIndex = NullableIndex.Null;
+                _core.SetResult( ValueTaskDelayResult.Cancelled );
+            }
+        }
+
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        internal ValueTaskDelayResult GetResult(in ConfiguredValueTaskAwaitable<ValueTaskDelayResult>.ConfiguredValueTaskAwaiter awaiter)
+        {
+            using ( Source.AcquireLock() )
+            {
+                try
+                {
+                    return awaiter.GetResult();
+                }
+                finally
+                {
+                    _core.Reset();
+                    if ( ! Source._isDisposed )
+                    {
+                        Assume.False( HeapIndex.HasValue );
+                        Assume.Equals( Timestamp, MaxTimestamp );
+                        Source._nodeCache.Push( this );
+                    }
+                }
+            }
+        }
+
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        internal static void CancelUnsafe(object? state)
+        {
+            Assume.IsNotNull( state );
+            var node = ReinterpretCast.To<Node>( state );
+            node.Cancel();
+        }
+
+        [MethodImpl( MethodImplOptions.AggressiveInlining )]
+        private void Cancel()
+        {
+            using ( Source.AcquireLock() )
+            {
+                if ( Source._isDisposed || ! HeapIndex.HasValue )
+                    return;
+
+                Source.RemoveFromSchedule( HeapIndex.Value );
+                SetCancelled();
+            }
+        }
+
+        ValueTaskDelayResult IValueTaskSource<ValueTaskDelayResult>.GetResult(short token)
+        {
+            return _core.GetResult( token );
+        }
+
+        ValueTaskSourceStatus IValueTaskSource<ValueTaskDelayResult>.GetStatus(short token)
+        {
+            return _core.GetStatus( token );
+        }
+
+        void IValueTaskSource<ValueTaskDelayResult>.OnCompleted(
+            Action<object?> continuation,
+            object? state,
+            short token,
+            ValueTaskSourceOnCompletedFlags flags)
+        {
+            _core.OnCompleted( continuation, state, token, flags );
+        }
+    }
+}
---
+++ tests/LfrlAnvil.Chrono.Tests/AsyncTests/ValueTaskDelaySourceTests.cs
@@ -0,0 +1,264 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using LfrlAnvil.Chrono.Async;
+using LfrlAnvil.Chrono.Internal;
+using LfrlAnvil.Functional;
+
+namespace LfrlAnvil.Chrono.Tests.AsyncTests;
+
+public class ValueTaskDelaySourceTests : TestsBase
+{
+    [Fact]
+    public async Task Schedule_ShouldReturnTaskWhichCompletesAccordingToScheduledDelay()
+    {
+        var timestamps = new TimestampProvider();
+        await using var sut = ValueTaskDelaySource.Start( timestamps );
+        var delay = Duration.FromMilliseconds( 15 );
+        var minTimestamp = timestamps.GetNow() + delay;
+        var task = sut.Schedule( delay );
+
+        var result = await task;
+
+        Assertion.All(
+                task.Owner.TestRefEquals( sut ),
+                timestamps.GetNow().TestGreaterThanOrEqualTo( minTimestamp ),
+                result.TestEquals( ValueTaskDelayResult.Completed ) )
+            .Go();
+    }
+
+    [Theory]
+    [InlineData( false )]
+    [InlineData( true )]
+    public async Task Schedule_ShouldReturnTaskWhichCompletesAccordingToScheduledDelay_WithConfigureAwait(bool continueOnCapturedContext)
+    {
+        var timestamps = new TimestampProvider();
+        await using var sut = ValueTaskDelaySource.Start( timestamps );
+        var delay = Duration.FromMilliseconds( 15 );
+        var minTimestamp = timestamps.GetNow() + delay;
+        var task = sut.Schedule( delay );
+
+        var result = await task.ConfigureAwait( continueOnCapturedContext );
+
+        Assertion.All(
+                task.Owner.TestRefEquals( sut ),
+                timestamps.GetNow().TestGreaterThanOrEqualTo( minTimestamp ),
+                result.TestEquals( ValueTaskDelayResult.Completed ) )
+            .Go();
+    }
+
+    [Fact]
+    public async Task Schedule_ShouldReturnTaskWhichCompletesWithCancelledResult_WhenCancellationTokenIsImmediatelyCancelled()
+    {
+        await using var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+        var result = await sut.Schedule( Duration.FromSeconds( 1 ), new CancellationToken( canceled: true ) );
+        result.TestEquals( ValueTaskDelayResult.Cancelled ).Go();
+    }
+
+    [Fact]
+    public async Task Schedule_ShouldReturnTaskWhichCompletesWithCancelledResult_WhenCancellationTokenIsCancelledBeforeDelayCompletion()
+    {
+        await using var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+        var cancellationSource = new CancellationTokenSource();
+        cancellationSource.CancelAfter( TimeSpan.FromMilliseconds( 15 ) );
+
+        var result = await sut.Schedule( Duration.FromSeconds( 1 ), cancellationSource.Token );
+
+        result.TestEquals( ValueTaskDelayResult.Cancelled ).Go();
+    }
+
+    [Fact]
+    public async Task
+        Schedule_ShouldReturnTaskWhichCompletesAccordingToScheduledDelay_WhenCancellationTokenIsCancelledAfterDelayCompletion()
+    {
+        var completion = new TaskCompletionSource();
+        await using var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+        var cancellationSource = new CancellationTokenSource();
+        cancellationSource.Token.Register( () => completion.SetResult() );
+        cancellationSource.CancelAfter( TimeSpan.FromMilliseconds( 50 ) );
+
+        var result = await sut.Schedule( Duration.FromMilliseconds( 15 ), cancellationSource.Token );
+        await completion.Task;
+
+        result.TestEquals( ValueTaskDelayResult.Completed ).Go();
+    }
+
+    [Theory]
+    [InlineData( -1 )]
+    [InlineData( 0 )]
+    public async Task Schedule_ShouldReturnTaskWhichCompletesImmediately_WhenDelayIsLessThanOrEqualToZero(int delayTicks)
+    {
+        await using var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+        var result = await sut.Schedule( Duration.FromTicks( delayTicks ) );
+        result.TestEquals( ValueTaskDelayResult.Completed ).Go();
+    }
+
+    [Theory]
+    [InlineData( -1 )]
+    [InlineData( 0 )]
+    public async Task
+        Schedule_ShouldReturnTaskWhichCompletesWithCancelledResult_WhenDelayIsLessThanOrEqualToZeroAndCancellationTokenIsImmediatelyCancelled(
+            int delayTicks)
+    {
+        await using var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+        var result = await sut.Schedule( Duration.FromTicks( delayTicks ), new CancellationToken( canceled: true ) );
+        result.TestEquals( ValueTaskDelayResult.Cancelled ).Go();
+    }
+
+    [Theory]
+    [InlineData( -1 )]
+    [InlineData( 0 )]
+    public async Task
+        Schedule_ShouldReturnTaskWhichCompletesImmediately_WhenDelayIsLessThanOrEqualToZeroAndCancellationTokenIsCancelledAfterDelayCompletion(
+            int delayTicks)
+    {
+        var completion = new TaskCompletionSource();
+        await using var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+        var cancellationSource = new CancellationTokenSource();
+        cancellationSource.Token.Register( () => completion.SetResult() );
+        cancellationSource.CancelAfter( TimeSpan.FromMilliseconds( 15 ) );
+
+        var result = await sut.Schedule( Duration.FromTicks( delayTicks ), cancellationSource.Token );
+        await completion.Task;
+
+        result.TestEquals( ValueTaskDelayResult.Completed ).Go();
+    }
+
+    [Fact]
+    public async Task Schedule_ShouldHandleMultipleDifferentDelaysCorrectly()
+    {
+        var completionOrder = new CompletionOrder();
+        await using var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+
+        var delay1 = sut.Schedule( Duration.FromMilliseconds( 90 ) );
+        var delay2 = sut.Schedule( Duration.FromMilliseconds( 30 ) );
+        var delay3 = sut.Schedule( Duration.FromMilliseconds( 60 ) );
+        var delay4 = sut.Schedule( Duration.FromMilliseconds( 150 ) );
+        var delay5 = sut.Schedule( Duration.FromMilliseconds( 120 ) );
+
+        var tasks = new[] { delay1, delay2, delay3, delay4, delay5 }.Select(
+                d =>
+                    Task.Factory.StartNew(
+                        async () =>
+                        {
+                            await d;
+                            return completionOrder.Next();
+                        } ) )
+            .ToArray();
+
+        var result = await Task.WhenAll( tasks );
+
+        result.Select( t => t.Result ).TestSequence( [ 3, 1, 2, 5, 4 ] ).Go();
+    }
+
+    [Fact]
+    public async Task Schedule_ShouldHandleMultipleDifferentDelaysCorrectly_WithCancellations()
+    {
+        var completionOrder = new CompletionOrder();
+        await using var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+        var cancellationSource1 = new CancellationTokenSource();
+        var cancellationSource2 = new CancellationTokenSource();
+        cancellationSource1.CancelAfter( TimeSpan.FromMilliseconds( 30 ) );
+        cancellationSource2.CancelAfter( TimeSpan.FromMilliseconds( 60 ) );
+
+        var delay1 = sut.Schedule( Duration.FromMilliseconds( 90 ) );
+        var delay2 = sut.Schedule( Duration.FromMilliseconds( 150 ), cancellationSource1.Token );
+        var delay3 = sut.Schedule( Duration.FromMilliseconds( 120 ) );
+        var delay4 = sut.Schedule( Duration.FromMilliseconds( 180 ) );
+        var delay5 = sut.Schedule( Duration.FromMilliseconds( 210 ) );
+        var delay6 = sut.Schedule( Duration.FromMilliseconds( 240 ) );
+        var delay7 = sut.Schedule( Duration.FromMilliseconds( 270 ), cancellationSource2.Token );
+
+        var tasks = new[] { delay1, delay2, delay3, delay4, delay5, delay6, delay7 }.Select(
+                d =>
+                    Task.Factory.StartNew(
+                        async () =>
+                        {
+                            var result = await d;
+                            return (completionOrder.Next(), result);
+                        } ) )
+            .ToArray();
+
+        var result = await Task.WhenAll( tasks );
+
+        result.Select( t => t.Result )
+            .TestSequence(
+            [
+                (3, ValueTaskDelayResult.Completed),
+                (1, ValueTaskDelayResult.Cancelled),
+                (4, ValueTaskDelayResult.Completed),
+                (5, ValueTaskDelayResult.Completed),
+                (6, ValueTaskDelayResult.Completed),
+                (7, ValueTaskDelayResult.Completed),
+                (2, ValueTaskDelayResult.Cancelled)
+            ] )
+            .Go();
+    }
+
+    [Fact]
+    public async Task Schedule_ShouldReturnTaskWhichCompletesWithDisposedResult_WhenSourceIsDisposed()
+    {
+        var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+        await sut.DisposeAsync();
+
+        var result = await sut.Schedule( Duration.Zero );
+
+        result.TestEquals( ValueTaskDelayResult.Disposed ).Go();
+    }
+
+    [Fact]
+    public async Task ThrowingTimestampProvider_ShouldSilentlyDisposeSource()
+    {
+        var sut = ValueTaskDelaySource.Start( new ThrowingTimestampProvider() );
+        await Task.Delay( 15 );
+
+        var result = await sut.Schedule( Duration.Zero );
+
+        result.TestEquals( ValueTaskDelayResult.Disposed ).Go();
+    }
+
+    [Fact]
+    public async Task Dispose_ShouldCompleteAllPendingDelaysWithDisposesResult()
+    {
+        var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+
+        var task1 = Task.Factory.StartNew( async () => await sut.Schedule( Duration.FromSeconds( 1 ) ) );
+        var task2 = Task.Factory.StartNew( async () => await sut.Schedule( Duration.FromSeconds( 1 ) ) );
+        var task3 = Task.Factory.StartNew( async () => await sut.Schedule( Duration.FromSeconds( 1 ) ) );
+
+        await Task.Delay( 15 );
+        await sut.DisposeAsync();
+        var result = await Task.WhenAll( task1, task2, task3 );
+
+        result.TestAll( (t, _) => t.Result.TestEquals( ValueTaskDelayResult.Disposed ) ).Go();
+    }
+
+    [Fact]
+    public void Dispose_ShouldNotThrow_WhenAlreadyDisposes()
+    {
+        var sut = ValueTaskDelaySource.Start( new TimestampProvider() );
+        sut.Dispose();
+
+        var action = Lambda.Of( () => sut.Dispose() );
+
+        action.Test( exc => exc.TestNull() ).Go();
+    }
+
+    private sealed class ThrowingTimestampProvider : TimestampProviderBase
+    {
+        public override Timestamp GetNow()
+        {
+            throw new Exception( "foo" );
+        }
+    }
+
+    private sealed class CompletionOrder
+    {
+        private int _current;
+
+        public int Next()
+        {
+            return Interlocked.Increment( ref _current );
+        }
+    }
+}
